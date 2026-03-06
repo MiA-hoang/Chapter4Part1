23a6a9d0f03579f185b00528af34af72795b8ded
NgocHa <hoangngocha11122k6@gmail.com>
2026-03-07 01:25:55 +0700

    Submit

---
+++ Bai6/Bai6/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Bai6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numA = new System.Windows.Forms.NumericUpDown();
+            this.numB = new System.Windows.Forms.NumericUpDown();
+            this.numC = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtThongTin = new System.Windows.Forms.RichTextBox();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.btnGiai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numA
+            // 
+            this.numA.Location = new System.Drawing.Point(80, 40);
+            this.numA.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numA.Name = "numA";
+            this.numA.Size = new System.Drawing.Size(120, 26);
+            this.numA.TabIndex = 0;
+            // 
+            // numB
+            // 
+            this.numB.Location = new System.Drawing.Point(333, 38);
+            this.numB.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numB.Name = "numB";
+            this.numB.Size = new System.Drawing.Size(120, 26);
+            this.numB.TabIndex = 1;
+            // 
+            // numC
+            // 
+            this.numC.Location = new System.Drawing.Point(594, 38);
+            this.numC.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numC.Name = "numC";
+            this.numC.Size = new System.Drawing.Size(120, 26);
+            this.numC.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(557, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "c";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "b";
+            // 
+            // txtThongTin
+            // 
+            this.txtThongTin.Location = new System.Drawing.Point(27, 100);
+            this.txtThongTin.Name = "txtThongTin";
+            this.txtThongTin.Size = new System.Drawing.Size(351, 215);
+            this.txtThongTin.TabIndex = 6;
+            this.txtThongTin.Text = "";
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(515, 127);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(100, 26);
+            this.txtX1.TabIndex = 7;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(515, 195);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(100, 26);
+            this.txtX2.TabIndex = 8;
+            // 
+            // btnGiai
+            // 
+            this.btnGiai.Location = new System.Drawing.Point(125, 344);
+            this.btnGiai.Name = "btnGiai";
+            this.btnGiai.Size = new System.Drawing.Size(135, 31);
+            this.btnGiai.TabIndex = 9;
+            this.btnGiai.Text = "Giải PTBH";
+            this.btnGiai.UseVisualStyleBackColor = true;
+            this.btnGiai.Click += new System.EventHandler(this.btnGiai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(378, 348);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(120, 32);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnGiai);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.txtThongTin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numC);
+            this.Controls.Add(this.numB);
+            this.Controls.Add(this.numA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numA;
+        private System.Windows.Forms.NumericUpDown numB;
+        private System.Windows.Forms.NumericUpDown numC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtThongTin;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.Button btnGiai;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Bai6/Bai6/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnGiai_Click(object sender, EventArgs e)
+        {
+            double a = (double)numA.Value;
+            double b = (double)numB.Value;
+            double c = (double)numC.Value;
+            txtThongTin.Text = "A = " + a + "\n";
+            txtThongTin.Text += "B = " + b + "\n";
+            txtThongTin.Text += "C = " + c + "\n";
+
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    txtThongTin.Text += "Phương trình vô nghiệm";
+                    txtX1.Clear();
+                    txtX2.Clear();
+                }
+                else
+                {
+                    double x = -c / b;
+                    txtThongTin.Text += "Phương trình có nghiệm: x = " + x;
+                    txtX1.Text = x.ToString();
+                    txtX2.Clear();
+                }
+            }
+            else
+            {
+                double delta = b * b - 4 * a * c;
+
+                if (delta < 0)
+                {
+                    txtThongTin.Text += "Phương trình vô nghiệm";
+                    txtX1.Clear();
+                    txtX2.Clear();
+                }
+                else if (delta == 0)
+                {
+                    double x = -b / (2 * a);
+                    txtThongTin.Text += "Phương trình có nghiệm kép";
+                    txtX1.Text = x.ToString("0.00");
+                    txtX2.Text = x.ToString("0.00");
+                }
+                else
+                {
+                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                    txtThongTin.Text += "Phương trình có 2 nghiệm phân biệt";
+                    txtX1.Text = x1.ToString("0.00");
+                    txtX2.Text = x2.ToString("0.00");
+                }
+            }
+        }
+    }
+}
